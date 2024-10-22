fe2ce5e8d4b9cae1f1100bbaf798f141b579026b
vimarupon@hotmail.com <vimarupon@gmail.com>
2024-10-22 11:14:31 +0200

    new changes

--- index.js
+++ index.js
@@ -8,7 +8,13 @@ const errorHandler = require("./src/middlewares/errorHandler");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://proyecto-10-full-stack-frontend.vercel.app",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 connectDB();
 
